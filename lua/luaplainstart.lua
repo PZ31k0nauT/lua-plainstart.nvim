89836ca1f381f8b9a230416ef3de9589792ee68f
PZ31k0-Linux <11.18656146e@gmail.com>
2021-07-12 23:01:31 +0200

    Added commands to luaplainstart.lua

--- lua/luaplainstart.lua
+++ lua/luaplainstart.lua
@@ -1,11 +1,32 @@
+local M = {}
+
+-- Create commands
+-- TODO: Was bedeuten bang und nargs
+local function create_commands()
+  vim.cmd("command! -bang -nargs=0 HelloWorldGerman :lua require('hello_world').german()" )
+  vim.cmd("command! -bang -nargs=0 HelloWorldEnglish :lua require('hello_world').english()" )
+end
+
+function M.german()
+  print("Hallo Welt")
+end
+
+function M.english()
+  print("Hello World")
+end
+
 local function say_hello()
   print("Hello World")
 end
 
-local function hello_world()
+function M.hello_world()
   say_hello()
 end
 
+function M.init()
+  create_commands()
+end
+
 return {
-  hello_world = hello_world
+  M
 }
